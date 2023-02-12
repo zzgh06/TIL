024205f54d54950bbe2ab45a3858cd050d2b129c
zzgh06 <zzgh06@naver.com>
2023-02-12 21:59:32 +0900

    코드업 기초 100제 풀이

---
+++ codeup/230212_codeup.py
@@ -0,0 +1,45 @@
+# 6051
+# a, b = map(int, input().split())
+# print('True' if a != b else 'False')
+
+# 6052
+# n = int(input())
+# print(bool(n))
+
+# 6053
+# a = bool(int(input()))
+# print(not a)
+
+# 6054
+# a, b = map(int, input().split())
+# print(bool(a) and bool(b))
+
+# 6055
+# a, b = map(int, input().split())
+# print(bool(a) or bool(b))
+
+# 6056
+# a, b = map(int, input().split())
+# c = bool(a)
+# d = bool(b)
+# print((c and (not d)) or ((not c) and d))
+
+# 6057
+# a, b = map(int, input().split())
+# c = bool(a)
+# d = bool(b)
+# print((not c and not d) or (c and d))
+
+# 6058
+# a, b = map(int, input().split())
+# c = bool(a)
+# d = bool(b)
+# print(not(c or d))
+
+# 6059
+# a = int(input())
+# print(~a)
+
+# 6060
+# a, b = map(int, input().split())
+# print(a & b)
