0dc09c021717b04acefb7dc515672a2b2df242dc
zzgh06 <zzgh06@naver.com>
2023-02-14 22:09:04 +0900

    swea 풀이

---
+++ SWEA/230214_swea.py
@@ -0,0 +1,13 @@
+# 2043. 서랍의 비밀번호
+# p, k = map(int, input().split())
+# cnt = 0
+# for i in range(p-k+1):
+#     cnt += 1
+# print(cnt)
+
+# 1933. 간단한 N 의 약수
+# n = int(input())
+
+# for i in range(1, n+1):
+#     if n%i == 0:
+#         print(i, end=' ')
