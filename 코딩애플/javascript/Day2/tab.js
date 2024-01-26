f16797659be9b1c0c26ac0baa36cb3cedfcda096
zzgh06 <zzgh06@naver.com>
2024-01-26 22:55:38 +0900

    240126-codingapple

--- 코딩애플/javascript/Day2/tab.js
+++ 코딩애플/javascript/Day2/tab.js
@@ -13,13 +13,39 @@ for (var i = 0; i < 3; i++){
 var 버튼 = $('.tab-button');
 var 내용 = $('.tab-content');
 // eq(0) == querySelectorAll('.tap-button')[0]
-for (let i = 0; i < 버튼.length; i++){
-  버튼.eq(i).on('click', function(){
-    버튼.removeClass('orange')
-    버튼.eq(i).addClass('orange')
-    내용.removeClass('show')
-    내용.eq(i).addClass('show')
-  });
+
+// for (let i = 0; i < 버튼.length; i++){
+//   버튼.eq(i).on('click', function(){
+//     // 버튼.removeClass('orange')
+//     // 버튼.eq(구멍).addClass('orange')
+//     // 내용.removeClass('show')
+//     // 내용.eq(구멍).addClass('show')
+//     탭열기(i)
+//   });
+// }
+
+$('.list').click(function(e){
+  // 지금 누른게 버튼 0이면 탭열기(0)
+  // if (e.target == document.querySelectorAll('.tab-button')[0]){
+  //   탭열기(0)
+  // }
+  // if (e.target == document.querySelectorAll('.tab-button')[1]){
+  //   탭열기(1)
+  // }
+  // if (e.target == document.querySelectorAll('.tab-button')[2]){
+  //   탭열기(2)
+  // }
+
+  // 이벤트리스너 1개만 써도 개발가능
+  탭열기(e.target.dataset.id)
+})
+
+// 함수로 축약할 때 변수있으면 파라미터로 바꿔보셈
+function 탭열기(구멍){
+  버튼.removeClass('orange')
+  버튼.eq(구멍).addClass('orange')
+  내용.removeClass('show')
+  내용.eq(구멍).addClass('show')
 }
 
 // 버튼.eq(1).on('click', function(){
